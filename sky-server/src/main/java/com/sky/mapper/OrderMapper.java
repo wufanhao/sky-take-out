31585d8f36c4f711759e1b87318ae8ae5ad7d61a
wufanhao <202521116012563@stu.hubu.edu.cn>
2026-04-08 02:06:34 +0000

    地址簿，用户下单，订单支付功能

---
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -0,0 +1,31 @@
+package com.sky.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.sky.entity.Orders;
+
+@Mapper
+public interface OrderMapper {
+
+    /**
+     * 插入订单数据
+     * @param orders
+     */
+    void insert(Orders orders);
+
+    
+    /**
+     * 根据订单号查询订单
+     * @param orderNumber
+     */
+    @Select("select * from orders where number = #{orderNumber}")
+    Orders getByNumber(String orderNumber);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+    
+}
